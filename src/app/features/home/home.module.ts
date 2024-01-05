6b55185d4d7a505817a05e3fd390f265cd7a5d10
Tata Kharatishvili <tata.kharatishvili@gmail.com>
2024-01-06 01:34:35 +0400

    add header

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { CoreModule } from 'src/app/core/core.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, CoreModule],
   exports: [HomeComponent],
 })
 export class HomeModule {}
