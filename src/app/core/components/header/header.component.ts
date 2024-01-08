8baaa69593df666bcf7eb6c97575e6cf3ca2e983
Tata Kharatishvili <tata.kharatishvili@gmail.com>
2024-01-08 05:52:07 +0400

    open signup dialog

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { SignupComponent } from 'src/app/features/auth/signup/signup.component';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(public dialog: MatDialog) {}
+
+  public openDialog() {
+    const dialogRef = this.dialog.open(SignupComponent, {
+      // width: '250px',
+      // data: {},
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+    });
+  }
+}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { SharedMaterialModule } from '../shared/shared-material/shared-material.module';
 
 @NgModule({
   declarations: [HeaderComponent, PageNotFoundComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedMaterialModule],
   exports: [HeaderComponent],
 })
 export class CoreModule {}
